ce8bfad1a4be291f058acc7c1bd8d3fa1bc1c653
GeoTV777 <ivushka777@mail.ru>
2024-02-16 21:19:56 +0300

    15/02/2024

--- src/main/java/pages/CatalogPage.java
+++ src/main/java/pages/CatalogPage.java
@@ -1,10 +1,28 @@
 package pages;
 
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class CatalogPage extends AbsBasePage{
 
     public CatalogPage(WebDriver driver) {
         super(driver, "/catalog/courses");
+    }
+
+    @FindBy(css = "div.bwGwUO a")
+    private List<WebElement> lessonsTiles;
+
+    public void lessonTilesNumberShouldBeSameAs(int number) {
+        Assertions.assertEquals(
+                number,
+                lessonsTiles.size(),
+                String.format("Numbers jf lessons tiles should be %d", number)
+        );
+
     }
 }
--- src/test/java/catalog/CorseTilesTest.java
+++ src/test/java/catalog/CorseTilesTest.java
@@ -1,12 +1,20 @@
 package catalog;
 
+import data.catalog.LessonsCategoryData;
+import exeption.BrowserNotSupportedExeption;
 import factory.DriverFactory;
 import factory.settings.ChromeDriverSettings;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import pages.CatalogPage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class CorseTilesTest {
 
@@ -15,10 +23,9 @@ public class CorseTilesTest {
 
 
     @BeforeEach
-    public void init() {
+        public void init() throws BrowserNotSupportedExeption{
         ChromeDriverSettings driverSettings = new ChromeDriverSettings();
         this.driver = new DriverFactory(driverSettings).create();
-        logger.info("Open browser");
     }
 
     @AfterEach
@@ -29,6 +36,14 @@ public class CorseTilesTest {
             logger.info("Close browser");
         }
     }
+    @Test
+    public void catalogTilesNumbers() {
+        List<String> queryParams = new ArrayList<>();
+        queryParams.add(String.format("categories=%s", LessonsCategoryData.TESTING.name().toLowerCase(Locale.ROOT)));
 
+        CatalogPage catalogPage = new CatalogPage(driver);
+        catalogPage.open(queryParams);
+        catalogPage.lessonTilesNumberShouldBeSameAs(10);
+    }
 
 }
